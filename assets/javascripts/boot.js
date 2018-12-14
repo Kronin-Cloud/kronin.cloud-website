f77b7370e06930f635a9bbc613d950a094261766
venhrun-petro <p.venhrun@pozitiff.com>
2018-12-14 13:59:57 +0200

    'minify_js'

--- assets/javascripts/boot.js
+++ assets/javascripts/boot.js
@@ -1,14 +1,2 @@
-(function($) {
-    "use strict";
-    $(document).ready(function() {
-        var $page = $('body');
-        var config = {
-          dependencyPath: {
-            plugin: 'javascripts/'
-          }
-        }
-        var application = new Tc.Application($page, config);
-        application.registerModules();
-        application.start();
-    });
-})(Tc.$);
+(function($){"use strict";$(document).ready(function(){var $page=$('body');var config={dependencyPath:{plugin:'javascripts/'}}
+var application=new Tc.Application($page,config);application.registerModules();application.start();});})(Tc.$);
